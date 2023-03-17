3d733e7512568c2d9f4f7a9f3332303537ecb3ec
Gabriel Martins <gabtec@sapo.pt>
2023-03-17 13:35:11 +0000

    feat: add new home page

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello Universe</h1>"))
+		w.Write([]byte("<h1>Hello Universe</h1><h4>CI/CD Running</h4>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
